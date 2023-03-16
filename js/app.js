7b3c2925fd2594c2881bfef26e172ffcd99daca7
jlap96 <lilyaaguilar9@gmail.com>
2023-03-16 14:46:11 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+//MENU RESPONSIVE
+
+var btnMenuOpen = document.getElementById("btnMenuOpen"),
+    btnMenuClose = document.getElementById("btnMenuClose"),
+    menuResponsive = document.getElementById("menuBar"),
+    enlaces = document.getElementById("enlaces");
+
+    //Click abrir
+    btnMenuOpen.addEventListener("click", function () {
+        menuResponsive.classList.add("active");
+    });
+
+    //Click cerrar
+    btnMenuClose.addEventListener("click", function () {
+        menuResponsive.classList.remove("active");
+    });
+
+    //Cerrar menu con elementos de enlaces
+    enlaces.addEventListener("click", function() {
+        menuResponsive.style.transitionDelay = "0.5s";
+        menuResponsive.classList.remove("active");
+    });
+
+
+//SLIDER DE PRODUCTOS
+var contenedor = document.querySelector('.slider'),
+    btnIzquierdo = document.getElementById("btn-izquierda"),
+    btnDerecho = document.getElementById("btn-derecha");
+
+    //EVENTO PARA EL BOTON DERECHO
+
+    btnDerecho.addEventListener("click", function() {
+        contenedor.scrollLeft += contenedor.offsetWidth;
+    });
+
+    //EVENTO PARA EL BOTON IZQUIERDO
+
+    btnIzquierdo.addEventListener("click", function() {
+        contenedor.scrollLeft -= contenedor.offsetWidth;
+    });
