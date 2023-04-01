91fd17218c370eca7c094ecd2171b39a5d8ff3e2
jlap96 <lilyaguilar9@gmail.com>
2023-03-31 18:10:44 -0600

    Functionality of the "go top" button were created

--- js/app.js
+++ js/app.js
@@ -78,3 +78,40 @@ var contenedor = document.querySelector('.slider'),
 
     // Eventos del formulario
     formulario.addEventListener("submit", validar);
+
+    //BOTON SCROLL TOP
+    var btnTop = document.getElementById("btn-top"),
+        logo = document.getElementById("logo");
+
+    //Detectar el scroll en la pagina web
+    window.addEventListener("scroll", function (){
+        var  scroll = document.documentElement.scrollTop,
+             fullSize = document.documentElement.offsetHeight,
+             sizeVP = document.documentElement.clientHeight;
+   
+   
+        if (scroll > 100) {
+             btnTop.classList.add("show");
+        }else{
+             btnTop.classList.remove("show");
+        }
+   
+        // MODIFICAR ELEMENTO CUANDO LLEGUE A FINAL DE PAGINA
+        if (fullSize == (scroll + sizeVP)) {
+            btnTop.classList.add("scrollFinal"); 
+        }else{
+            btnTop.classList.remove("scrollFinal");
+        }
+   });
+   
+   
+   // DETECTAMOS EVENTO CLICK EN EL BOTON
+   btnTop.addEventListener("click", function () {
+        window.scrollTo(0,0);
+   });
+
+   //DETECTAR EVENTO EN LOGO
+   logo.addEventListener("click", function () {
+    window.scrollTo(0,0);
+});
+   
